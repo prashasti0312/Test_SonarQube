0675517510fdaa180285adc78db936dacb94ff4a
Prashasti Gupta <princess.prashastigupta@gmail.com>
2020-09-02 18:28:05 +0530

    Commit 1

---
+++ CI-CD-Lab2/src/main/java/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+	private int a=10;
+	public void func(int n)
+	{
+	 n=10;
+	}
+public static void main(String args[])
+{
+	int b=20;
+	System.out.println("Hello SonarQube");
+	if(b==10)
+	{
+		System.out.println("False");
+	}
+}
+}
