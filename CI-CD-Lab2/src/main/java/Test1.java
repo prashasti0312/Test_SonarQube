6f089e6746d382cf19c15e3c9bb3b2d5ca2d04f0
Prashasti Gupta <princess.prashastigupta@gmail.com>
2022-03-01 11:50:23 +0530

    Update Test1.java

--- CI-CD-Lab2/src/main/java/Test1.java
+++ CI-CD-Lab2/src/main/java/Test1.java
@@ -7,11 +7,17 @@ public class Test1 {
 public static void main(String args[])
 {
 	int b=20;
+	int c=15;
+	int d=25;
 	System.out.println("Hello SonarQube");
 	if(b==10)
 	{
 		System.out.println("Incorrect");
 	}
 }
+	public void func()
+	{
+	 int n1=10;
+	}
 }
 
